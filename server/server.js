3564819f9378548929ea94db38e20de45a439c71
Bhumisha Dave <bhumishadave@yahoo.com>
2021-05-01 22:00:48 -0700

    Add thought and friend

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
-
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -12,7 +12,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context:authMiddleware
 });
 server.applyMiddleware({ app });
 
